b5067306ffc2af08bccc97d07370293041e54755
Fadoua Ait Amrou <i@saamer.me>
2022-01-18 03:08:43 -0500

    Reformatted MainJS, Changed logo, Started Countdown

--- js/main.js
+++ js/main.js
@@ -1,247 +1,251 @@
-$(function() {
-    "use strict";
+$(function () {
+  "use strict";
 
-
-
-
-	/* ==========================================================================
+  /* ==========================================================================
        Sub Form   
        ========================================================================== */
 
+  $("#mc-form").ajaxChimp({
+    callback: callbackFunction,
+    url: "http://csmthemes.us3.list-manage.com/subscribe/post?u=9666c25a337f497687875a388&id=45b2c44b98",
+    // http://xxx.xxx.list-manage.com/subscribe/post?u=xxx&id=xxx
+  });
 
-	$('#mc-form').ajaxChimp({
-		callback: callbackFunction,
-		url: 'http://csmthemes.us3.list-manage.com/subscribe/post?u=9666c25a337f497687875a388&id=45b2c44b98'
-			// http://xxx.xxx.list-manage.com/subscribe/post?u=xxx&id=xxx
-	});
+  function callbackFunction(resp) {
+    if (resp.result === "success") {
+      $(".alert").slideUp();
+      $(".success").slideDown();
+    } else if (resp.result === "error") {
+      $(".success").slideUp();
+      $(".alert").slideDown();
+    }
+  }
 
-
-	function callbackFunction(resp) {
-		if (resp.result === 'success') {
-			$('.alert').slideUp();
-			$('.success').slideDown();
-		} else if (resp.result === 'error') {
-			$('.success').slideUp();
-			$('.alert').slideDown();
-		}
-	}
-
-
-
-
-
-    /* ==========================================================================
+  /* ==========================================================================
    Preload
    ========================================================================== */
-    
-    $(window).load(function() {
-        
-        $("#status").fadeOut();
-        
-        $("#preloader").delay(1000).fadeOut("slow");
-    });
 
+  $(window).load(function () {
+    $("#status").fadeOut();
 
-    /* ==========================================================================
+    $("#preloader").delay(1000).fadeOut("slow");
+  });
+
+  /* ==========================================================================
    On Scroll animation
    ========================================================================== */
-    
-    if ($(window).width() > 992) {
-        new WOW().init();
-    }
 
-    /* ==========================================================================
+  if ($(window).width() > 992) {
+    new WOW().init();
+  }
+
+  /* ==========================================================================
    Smooth Scroll
    ========================================================================== */
-    
-    $('a[href*=#]:not([href=#])').click(function() {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html,body').animate({
-                    scrollTop: (target.offset().top - 40)
-                }, 1000);
-                return false;
-            }
-        }
-    });
 
+  $("a[href*=#]:not([href=#])").click(function () {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        $("html,body").animate(
+          {
+            scrollTop: target.offset().top - 40,
+          },
+          1000
+        );
+        return false;
+      }
+    }
+  });
 
-
-    /* ==========================================================================
+  /* ==========================================================================
    App screenshot slider
    ========================================================================== */
-    
-    $(".screenshot-slider").owlCarousel({
-        autoPlay: 3000, //Set AutoPlay speed
-        items: 4,
-        itemsDesktop: [1199, 3],
-        itemsDesktopSmall: [979, 3]
-    });
 
-    /* ==========================================================================
+  $(".screenshot-slider").owlCarousel({
+    autoPlay: 3000, //Set AutoPlay speed
+    items: 4,
+    itemsDesktop: [1199, 3],
+    itemsDesktopSmall: [979, 3],
+  });
+
+  /* ==========================================================================
    Tweet
    ========================================================================== */
-    
-    
-    $('.tweet').twittie({
-        username: 'envatomarket', // change username here
-        dateFormat: '%b. %d, %Y',
-        template: '{{tweet}} {{user_name}}',
-        count: 10
-    }, function() {
-        var item = $('.tweet ul');
-        
-        item.children('li').first().show().siblings().hide();
-        setInterval(function() {
-            item.find('li:visible').fadeOut(500, function() {
-                $(this).appendTo(item);
-                item.children('li').first().fadeIn(500);
-            });
-        }, 5000);
-    });
 
-    /* ==========================================================================
+  $(".tweet").twittie(
+    {
+      username: "envatomarket", // change username here
+      dateFormat: "%b. %d, %Y",
+      template: "{{tweet}} {{user_name}}",
+      count: 10,
+    },
+    function () {
+      var item = $(".tweet ul");
+
+      item.children("li").first().show().siblings().hide();
+      setInterval(function () {
+        item.find("li:visible").fadeOut(500, function () {
+          $(this).appendTo(item);
+          item.children("li").first().fadeIn(500);
+        });
+      }, 5000);
+    }
+  );
+
+  /* ==========================================================================
    countdown
    ========================================================================== */
-    
-    $('.countdown').downCount({
-        date: '10/01/2021 12:00:00' // m/d/y
-    });
 
+  $(".countdown").downCount({
+    date: "04/02/2022 12:00:00", // m/d/y
+  });
 
-
-
-
-    /* ==========================================================================
+  /* ==========================================================================
        Contact Form
        ========================================================================== */
-    
-    
-    $('#contact-form').validate({
-        rules: {
-            name: {
-                required: true,
-                minlength: 2
-            },
-            email: {
-                required: true,
-                email: true
-            },
-            
-            message: {
-                required: true,
-                minlength: 10
-            }
-        },
-        messages: {
-            name: "<i class='fa fa-exclamation-triangle'></i>Please specify your name.",
-            email: {
-                required: "<i class='fa fa-exclamation-triangle'></i>We need your email address to contact you.",
-                email: "<i class='fa fa-exclamation-triangle'></i>Please enter a valid email address."
-            },
-            message: "<i class='fa fa-exclamation-triangle'></i>Please enter your message."
-        },
-        submitHandler: function(form) {
-            $(form).ajaxSubmit({
-                type: "POST",
-                data: $(form).serialize(),
-                url: "php/contact-me.php",
-                success: function() {
-                    $('#contact-form :input').attr('disabled', 'disabled');
-                    $('#contact-form').fadeTo("slow", 0.15, function() {
-                        $(this).find(':input').attr('disabled', 'disabled');
-                        $(this).find('label').css('cursor', 'default');
-                        $('.success-cf').fadeIn();
-                    });
-                    $('#contact-form')[0].reset();
-                },
-                error: function() {
-                    $('#contact-form').fadeTo("slow", 0.15, function() {
-                        $('.error-cf').fadeIn();
-                    });
-                }
-            });
-        }
-    });
 
-    /* ==========================================================================
+  $("#contact-form").validate({
+    rules: {
+      name: {
+        required: true,
+        minlength: 2,
+      },
+      email: {
+        required: true,
+        email: true,
+      },
+
+      message: {
+        required: true,
+        minlength: 10,
+      },
+    },
+    messages: {
+      name: "<i class='fa fa-exclamation-triangle'></i>Please specify your name.",
+      email: {
+        required:
+          "<i class='fa fa-exclamation-triangle'></i>We need your email address to contact you.",
+        email:
+          "<i class='fa fa-exclamation-triangle'></i>Please enter a valid email address.",
+      },
+      message:
+        "<i class='fa fa-exclamation-triangle'></i>Please enter your message.",
+    },
+    submitHandler: function (form) {
+      $(form).ajaxSubmit({
+        type: "POST",
+        data: $(form).serialize(),
+        url: "php/contact-me.php",
+        success: function () {
+          $("#contact-form :input").attr("disabled", "disabled");
+          $("#contact-form").fadeTo("slow", 0.15, function () {
+            $(this).find(":input").attr("disabled", "disabled");
+            $(this).find("label").css("cursor", "default");
+            $(".success-cf").fadeIn();
+          });
+          $("#contact-form")[0].reset();
+        },
+        error: function () {
+          $("#contact-form").fadeTo("slow", 0.15, function () {
+            $(".error-cf").fadeIn();
+          });
+        },
+      });
+    },
+  });
+
+  /* ==========================================================================
    ScrollTop Button
    ========================================================================== */
-    
-    
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 200) {
-            $('.scroll-top a').fadeIn(200);
-        } else {
-            $('.scroll-top a').fadeOut(200);
-        }
-    });
-    
-    
-    $('.scroll-top a').click(function(event) {
-        event.preventDefault();
-        
-        $('html, body').animate({
-            scrollTop: 0
-        }, 1000);
-    });
 
-    /* ==========================================================================
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 200) {
+      $(".scroll-top a").fadeIn(200);
+    } else {
+      $(".scroll-top a").fadeOut(200);
+    }
+  });
+
+  $(".scroll-top a").click(function (event) {
+    event.preventDefault();
+
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      1000
+    );
+  });
+
+  /* ==========================================================================
    parallax scrolling 
    ========================================================================== */
-    
-    
-		
-		if (!(/Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i).test(navigator.userAgent || navigator.vendor || window.opera)) {
-        		if($(window).width()>992){skrollr.init({forceHeight:false})}$(window).on("resize",function(){if($(window).width()<=	992){skrollr.init().destroy()}});$(window).on("resize",function(){if($(window).width()>992){skrollr.init({forceHeight:false})}});
-    }
 
-    /* ==========================================================================
+  if (
+    !/Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i.test(
+      navigator.userAgent || navigator.vendor || window.opera
+    )
+  ) {
+    if ($(window).width() > 992) {
+      skrollr.init({ forceHeight: false });
+    }
+    $(window).on("resize", function () {
+      if ($(window).width() <= 992) {
+        skrollr.init().destroy();
+      }
+    });
+    $(window).on("resize", function () {
+      if ($(window).width() > 992) {
+        skrollr.init({ forceHeight: false });
+      }
+    });
+  }
+
+  /* ==========================================================================
    sticky nav
    ========================================================================== */
-    
-    
-    
-    var menu = $('.navbar');
-    
-    var stickyNav = menu.offset().top;
-    
-    $(window).scroll(function() {
-        if ($(window).scrollTop() > $(window).height()) {
-            menu.addClass('stick');
-        } else {
-            menu.removeClass('stick');
-        
-        }
-    });
-		
-		
-    /* ==========================================================================
+
+  var menu = $(".navbar");
+
+  var stickyNav = menu.offset().top;
+
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > $(window).height()) {
+      menu.addClass("stick");
+    } else {
+      menu.removeClass("stick");
+    }
+  });
+
+  /* ==========================================================================
    Fade
    ========================================================================== */
-    
-    $(window).scroll(function(e) {
-        var s = $(window).scrollTop(), 
-        d = $(document).height(), 
-        c = $(window).height(), 
-        opacityVal = (s / 400);
-        $('.main .overlay').css('opacity', opacityVal);
-    });			
 
+  $(window).scroll(function (e) {
+    var s = $(window).scrollTop(),
+      d = $(document).height(),
+      c = $(window).height(),
+      opacityVal = s / 400;
+    $(".main .overlay").css("opacity", opacityVal);
+  });
 
-	/* ==========================================================================
+  /* ==========================================================================
 	   Collapse nav bar
 	   ========================================================================== */
-	
 
-	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
- 			$(".navbar-nav li a").on('click', function() {
-	    $(".navbar-collapse").collapse('hide');
-	});
-}
-
-
-
+  if (
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    )
+  ) {
+    $(".navbar-nav li a").on("click", function () {
+      $(".navbar-collapse").collapse("hide");
+    });
+  }
 });
